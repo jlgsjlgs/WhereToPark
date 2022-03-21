2e1875ca3a04385fdcc96b0850ed4e0ae45dbdb8
Klee057 <tojablejinx@gmail.com>
2022-03-21 14:06:41 +0800

    added mappage.js

---
+++ js/mappage.js
@@ -0,0 +1,25 @@
+let autocomplete;
+function initMap() {
+        let mapOptions = {center: {lat: 1.3483, lng: 103.6831},
+            zoom: 15};
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var input = document.getElementById('search_term');
+
+        autocomplete = new google.maps.places.Autocomplete(input,{
+        types: ['establishment'],
+        componentRestrictions: {'country': ['SG']},
+        fields: ['place_id', 'geometry', 'name']
+    });
+        // Linking startuppage search result to mappage
+        var tempitem = sessionStorage.getItem("locationmarker");
+    
+        if (tempitem != null) {
+            sessionStorage.clear();
+            var searchresult = JSON.parse(tempitem);
+            map.panTo(new google.maps.Marker({
+                position: searchresult.geometry.location,
+                title: searchresult.name,
+                map: map
+            }).position);
+        }
+    }
