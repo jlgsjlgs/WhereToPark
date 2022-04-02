ddcc3b98cae9fdbe2f2d8bf0c60daeec51f88def
sadcsundergrad <58691416+jlgsjlgs@users.noreply.github.com>
2022-04-02 18:55:27 +0800

    Added button for simulating user login features

--- js/mappage.js
+++ js/mappage.js
@@ -1,6 +1,5 @@
 getCarparkLocations(); //STARTS EVERYTHING IN MAPPAGE.JS
 
-
 const api_govforgmap = 'https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c&limit=5000';
 
 var resultLatLng = new Array();
